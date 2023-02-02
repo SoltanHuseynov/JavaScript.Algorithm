cf988cdcf5160e8aa4e588ac72a06b9ae1ab0c23
SoltanHuseynov <sayepzero@gmail.com>
2023-02-03 02:13:40 +0300

    Search

--- src/InterpolationSearch.js
+++ src/InterpolationSearch.js
@@ -1,3 +1,8 @@
 /*
-	
+  -> it's similer to binary search.
+  -> it instead of dividing the array into two parts in each iteration,
+     it uses an estimation of the position of 
+ 	 the target value based on the range of values in the array.
+
+ 	 position=low+( (key-arr[low])*(hig-low) / (arr[high]-arr[low]) )
 */
